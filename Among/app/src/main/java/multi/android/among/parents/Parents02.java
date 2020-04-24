ba435c4d82997813f855a911984c7e4a8b0245c3
kim-svaodz <dhkdghehfdl@gmail.com>
2020-04-24 18:32:44 +0900

    git

--- Among/app/src/main/java/multi/android/among/parents/Parents02.java
+++ Among/app/src/main/java/multi/android/among/parents/Parents02.java
@@ -2,15 +2,31 @@ package multi.android.among.parents;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import multi.android.among.R;
 
 public class Parents02 extends AppCompatActivity {
+    Button close_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parents02);
+        close_btn = findViewById(R.id.close_btn);
+        final Intent intent = getIntent();
+
+        close_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //종료시 putExtra 추가 기능을 위한 값을 전달해주기
+
+
+                finish();
+            }
+        });
     }
 }
